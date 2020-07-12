4c144dcfadd5329a826d19881f8f0ce4f29bd19f
toniktoo <toniktoo@mail.ru>
2020-07-12 22:13:50 +0300

    Updates

---
+++ precache-manifest.89e18d510b6f67bef0346c91130abf24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60089d6a176bbba8a6cf0c5e821a529b",
+    "url": "/headhunter/index.html"
+  },
+  {
+    "revision": "cb40fcb76895eb6774b2",
+    "url": "/headhunter/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "7d3bc421b5b106959b44",
+    "url": "/headhunter/static/css/main.f4fd829c.chunk.css"
+  },
+  {
+    "revision": "cb40fcb76895eb6774b2",
+    "url": "/headhunter/static/js/2.b9dbee60.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/headhunter/static/js/2.b9dbee60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3bc421b5b106959b44",
+    "url": "/headhunter/static/js/main.db0dad2f.chunk.js"
+  },
+  {
+    "revision": "9f0e8aa01dc42e10a5f2",
+    "url": "/headhunter/static/js/runtime-main.9e80c5fc.js"
+  }
+]);
