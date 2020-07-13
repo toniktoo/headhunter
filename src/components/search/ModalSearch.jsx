91bb4fc2ff6bb8ce110c066c2fc0e7bd3f232944
toniktoo <46635218+toniktoo@users.noreply.github.com>
2020-07-13 19:19:54 +0300

    Update ModalSearch.jsx

--- src/components/search/ModalSearch.jsx
+++ src/components/search/ModalSearch.jsx
@@ -115,7 +115,6 @@ export const ModalSearch = () => {
   }, [currentPage]);
 
   const handleSubmit = async () => {
-    setTextValue('');
     dispatch(setFirstLoaded({ firstLoadedApp: true }));
     await fetchSnippetVacancies(textValue, city.areaId);
     await toggleSettings();
